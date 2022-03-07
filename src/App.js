1d407a726da848476f3aa0fbf2d8a92deb3341c5
plexor <rcolon85@gmail.com>
2022-03-07 16:10:55 -0500

    master creation of weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'init',
+      isLoaded: false,
+      weatherData: null
+    }
+  }
+
+
+
+  // second for controller
+  abortController = new AbortController();
+  controllerSignal = this.abortController.signal;
+
+
+
+//  first function added Start
+getWeatherData = () => {
+  const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=d9b9c171ecd284c4d7b079bf76a5ee51`;
+
+  fetch(weatherApi, { signal: this.controllerSignal })
+  .then(response => response.json())
+  .then(
+    (result) => {
+
+      console.log(result);
+
+      const { name } = result;
+      const { country } = result.sys;
+      const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+      const { description, icon } = result.weather[0];
+      const { speed, deg } = result.wind;
+
+      this.setState({
+        isLoaded: true,
+        weatherData: {
+          name,
+          country,
+          description,
+          icon,
+          temp: temp.toFixed(1),
+          feels_like: feels_like.toFixed(1),
+          temp_min: temp_min.toFixed(1),
+          temp_max: temp_max.toFixed(1),
+          speed,
+          deg,
+          humidity
+        }
+      });
+    },
+    (error) => {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
   );
+}
+//end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//third
+componentDidMount() {
+   this.getWeatherData();
+}
+
+componentWillUnmount() {
+  this.abortController.abort();
+}
+
+//end
+
+// add all code above render, variable and functions dont go in return! 
+  render() {
+    return (
+      <div className='App'>
+        <div className='container'>
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
