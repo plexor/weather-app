ba4f9699bf71589cccd71f492b7951709f9a2a4d
plexor <rcolon85@gmail.com>
2022-03-08 00:43:07 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.scss';
+
 import { WeatherData } from './components/WeatherData'
-
-
-
+import { StatusData } from './components/StatusData'
 
 class App extends React.Component {
   constructor(props) {
@@ -15,88 +14,113 @@ class App extends React.Component {
     }
   }
 
-
-
-  // second for controller
   abortController = new AbortController();
   controllerSignal = this.abortController.signal;
 
+  weatherInit = () => {
 
-
-//  first function added Start
-getWeatherData = () => {
-  const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=d9b9c171ecd284c4d7b079bf76a5ee51`;
-
-  fetch(weatherApi, { signal: this.controllerSignal })
-  .then(response => response.json())
-  .then(
-    (result) => {
-
-      console.log(result);
-
-      const { name } = result;
-      const { country } = result.sys;
-      const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
-      const { description, icon } = result.weather[0];
-      const { speed, deg } = result.wind;
-
-      this.setState({
-        isLoaded: true,
-        weatherData: {
-          name,
-          country,
-          description,
-          icon,
-          temp: temp.toFixed(1),
-          feels_like: feels_like.toFixed(1),
-          temp_min: temp_min.toFixed(1),
-          temp_max: temp_max.toFixed(1),
-          speed,
-          deg,
-          humidity
-        }
-      });
-    },
-    (error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
+    const success = (position) => {
+      this.setState({status: 'fetching'});
+      localStorage.setItem('location-allowed', true);
+      this.getWeatherData(position.coords.latitude, position.coords.longitude);
     }
-  );
-}
-//end
+    
+    const error = () => {
+      this.setState({status: 'unable'});
+      localStorage.removeItem('location-allowed');
+      alert('Unable to retrieve location.');
+    }
+    
+    if (navigator.geolocation) {
+      this.setState({status: 'fetching'});
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      this.setState({status: 'unsupported'});
+      alert('Your browser does not support location tracking, or permission is denied.');
+    }
+  }
+
+  getWeatherData = (lat, lon) => {
+    const weatherApi = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=d9b9c171ecd284c4d7b079bf76a5ee51`;
+
+    fetch(weatherApi, { signal: this.controllerSignal })
+    .then(response => response.json())
+    .then(
+      (result) => {
+        console.log(result);
+        const { name } = result;
+        const { country } = result.sys;
+        const { temp, temp_min, temp_max, feels_like, humidity } = result.main;
+        const { description, icon } = result.weather[0];
+        const { speed, deg } = result.wind;
+
+        this.setState({
+          status: 'success',
+          isLoaded: true,
+          weatherData: {
+            name,
+            country,
+            description,
+            icon,
+            temp: temp.toFixed(1),
+            feels_like: feels_like.toFixed(1),
+            temp_min: temp_min.toFixed(1),
+            temp_max: temp_max.toFixed(1),
+            speed,
+            deg,
+            humidity
+          }
+        });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    );
+  }
+  
+  onClick = () => {
+    this.weatherInit();
+  }
+
+  returnActiveView = (status) => {
+    switch(status) {
+      case 'init':
+        return(
+          <button 
+          className='btn-main' 
+          onClick={this.onClick}
+          >
+            Get My Location
+          </button>
+        );
+      case 'success':
+        return <WeatherData data={this.state.weatherData} />;
+      default:
+        return <StatusData status={status} />;
+    }
+  }
 
 
+  componentDidMount() {
+    if(localStorage.getItem('location-allowed')) {
+      this.weatherInit();
+    } else {
+      return;
+    }
+  }
 
+  componentWillUnmount() {
+    this.abortController.abort();
+  }
 
-
-
-
-
-
-
-
-
-
-
-//third
-componentDidMount() {
-   this.getWeatherData();
-}
-
-componentWillUnmount() {
-  this.abortController.abort();
-}
-
-//end
-
-// add all code above render, variable and functions dont go in return! 
   render() {
     return (
       <div className='App'>
         <div className='container'>
-        <WeatherData />
+          {this.returnActiveView(this.state.status)}
         </div>
       </div>
     );
